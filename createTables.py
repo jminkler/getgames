2eb8f2fbe88eb21c827606dfc6ce0372ec4515b6
Jarret Minkler <jarret@domyownpestcontrol.com>
2019-11-03 19:45:19 -0500

    add table script

---
+++ createTables.py
@@ -0,0 +1,26 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.create_table(
+    TableName='pgns',
+    KeySchema=[
+        {
+            'AttributeName': 'hash', 
+            'KeyType': 'HASH'
+        }
+    ], 
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'hash', 
+            'AttributeType': 'S'
+        }
+    ], 
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1, 
+        'WriteCapacityUnits': 1
+    }
+)
+
+table.meta.client.get_waiter('table_exists').wait(TableName='staff')
+print(table.item_count)
